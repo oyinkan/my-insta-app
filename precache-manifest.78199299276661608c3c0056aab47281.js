f25867b2448f5d355d2cb74220e35628d885be0b
oyinkan <skusman@student.lautech.edu.ng>
2020-04-08 14:19:16 +0100

    Updates

---
+++ precache-manifest.78199299276661608c3c0056aab47281.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b99052a15816a77e0ad8aa7f715cc4db",
+    "url": "/using-react-router/index.html"
+  },
+  {
+    "revision": "71c0cff729ba99f05779",
+    "url": "/using-react-router/static/css/main.e1141b0c.chunk.css"
+  },
+  {
+    "revision": "79142221d2eee60ba5bc",
+    "url": "/using-react-router/static/js/2.88b38c39.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/using-react-router/static/js/2.88b38c39.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "71c0cff729ba99f05779",
+    "url": "/using-react-router/static/js/main.2a8030b2.chunk.js"
+  },
+  {
+    "revision": "2071f6be042913830972",
+    "url": "/using-react-router/static/js/runtime-main.279f8cf1.js"
+  }
+]);
